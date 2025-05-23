44c8a62c3647d69c9631027e4d835836520c5bc5
sujit1517 <nagmodesujit15@gmail.com>
2025-05-23 11:08:31 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "MyInstance"
+  }
+}
+output "my_ip" {
+value = aws_instance.myec2.public_ip
+}
